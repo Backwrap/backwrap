340d712b519e04ea3c9de93bef7cd376b625d4b0
excellence-arch <oladipupomichael9@gmail.com>
2022-10-07 10:37:05 +0300

    setting up the backwrap package

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
-const createApp = require("./utils");
+const app = require("express")();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
 
-createApp();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(cors());
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`App is listening on ${PORT}`);
+});

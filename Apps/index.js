64e187b752c542eb8783ed20b3bb1bc7014fbaab
excellence-arch <oladipupomichael9@gmail.com>
2022-10-07 18:46:02 +0300

    now add dynamic routes

---
+++ Apps/index.js
@@ -0,0 +1,7 @@
+	
+const igdRouter = require("./igd/index")
+const kjsdaRouter = require("./kjsda/index")
+const kjdfsRouter = require("./kjdfs/index")
+const kjdsRouter = require("./kjds/index")
+const djksRouter = require("./djks/index")
+module.exports = {igdRouter, kjsdaRouter, kjdfsRouter, kjdsRouter, djksRouter, }
--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const app = require("express")();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const fs = require("fs");
+const apps = require("./Apps/index");
 require("dotenv").config();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(cors());
+
+fs.readFile("apps.json", (err, data) => {
+  let json = JSON.parse(data);
+  for (i of json) {
+    let router = `${i.imports}Router`;
+    app.use(`/api/${router}`, apps[router]);
+  }
+});
 
 const PORT = process.env.PORT || 8080;
 

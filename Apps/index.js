979295cb99e14ea152f406632b593372f89b1760
excellence-arch <oladipupomichael9@gmail.com>
2022-10-08 21:31:12 +0300

    now creating a new app

--- Apps/index.js
+++
@@ -1,7 +0,0 @@
-	
-const igdRouter = require("./igd/index")
-const kjsdaRouter = require("./kjsda/index")
-const kjdfsRouter = require("./kjdfs/index")
-const kjdsRouter = require("./kjds/index")
-const djksRouter = require("./djks/index")
-module.exports = {igdRouter, kjsdaRouter, kjdfsRouter, kjdsRouter, djksRouter, }
